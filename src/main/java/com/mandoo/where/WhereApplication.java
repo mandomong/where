11f530be96c2675f0e37ac0a7fbe4bd382911658
mandoomong <orangetreex@gmail.com>
2020-05-04 11:39:45 +0900

    20200504 hyemin properties 암호화 추가 (jasypt lib)

--- src/main/java/com/mandoo/where/WhereApplication.java
+++ src/main/java/com/mandoo/where/WhereApplication.java
@@ -1,5 +1,6 @@
 package com.mandoo.where;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class WhereApplication {
 
 	public static void main(String[] args) {
