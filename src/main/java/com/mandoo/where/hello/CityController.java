b6d8bea26e7e78bfc3669bf8176b6349d4c9be7a
mandoomong <orangetreex@gmail.com>
2020-06-29 23:05:25 +0900

    Revert "20200503 hyemin update index page"
    This reverts commit 516a982c

--- src/main/java/com/mandoo/where/hello/CityController.java
+++ src/main/java/com/mandoo/where/hello/CityController.java
@@ -5,7 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -26,7 +28,7 @@ public class CityController {
 
         List<City> cities = (List<City>) cityService.findAll();
         model.addAttribute("cities", cities);
-        //System.out.println(cities);
+        System.out.println(cities);
         return "showCities";
 
     }
--- src/main/java/com/mandoo/where/hello/Hello.java
+++ src/main/java/com/mandoo/where/hello/Hello.java
@@ -1,6 +1,9 @@
 package com.mandoo.where.hello;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
